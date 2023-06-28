64fdde3e28e15ada404360399cef831280f2845d
Pastilhex <ivodanielmarques@gmail.com>
2023-06-28 14:49:52 +0100

    new file

---
+++ src/lexer/ft_count_args.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_count_args.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ialves-m <ialves-m@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/28 14:46:42 by ialves-m          #+#    #+#             */
+/*   Updated: 2023/06/28 14:47:06 by ialves-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+void	ft_count_args(t_ms *ms, t_elem *list)
+{
+	int	i;
+	int j;
+
+	i = 0;
+	j = 0;
+	ms->n_pipes = ft_count_pipes(list);
+	ms->count_args = malloc((ms->n_pipes + 1) * sizeof(int));
+	while (list)
+	{
+		if (list->type == SINGLE_QUOTE)
+		{
+			list = list->next;
+			if (list->status == IN_SQUOTE)
+				j++;
+			while (list->type != SINGLE_QUOTE)
+				list = list->next;
+			list = list->next;
+		}
+		else if (list->type == DOUBLE_QUOTE)
+		{
+			list = list->next;
+			if (list->status == IN_DQUOTE)
+				j++;
+			while (list->type != DOUBLE_QUOTE)
+				list = list->next;
+			list = list->next;
+		}
+		else if (list->type == WORD)
+		{
+			j++;
+			list = list->next;
+		}
+		else if (list->type == PIPE_LINE)
+		{
+			ms->count_args[i++] = j;
+			j = 1;
+			list = list->next;
+			ms->count_args[i++] = j;
+			j= 0;
+		}
+		else if (ft_is_redir(list->type))
+		{
+			while (list && list->type != PIPE_LINE)
+				list = list->next;
+		}
+		else
+			list = list->next;
+	}
+	ms->count_args[i] = j;
+}
