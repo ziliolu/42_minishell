1cf58ec8dac12e75639dd3aec874e07e148bba75
Pastilhex <ivodanielmarques@gmail.com>
2023-07-26 12:34:58 +0100

    testing LLONG

--- src/builtins/ft_exit_validation.c
+++ src/builtins/ft_exit_validation.c
@@ -6,7 +6,7 @@
 /*   By: ialves-m <ialves-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 13:05:23 by lpicoli-          #+#    #+#             */
-/*   Updated: 2023/07/26 07:30:25 by ialves-m         ###   ########.fr       */
+/*   Updated: 2023/07/26 12:30:25 by ialves-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,19 @@
 
 bool	ft_exit_validation(t_ms *ms, t_command *cmd)
 {
-	int		i;
+	//int		i;
 	long long int	nbr;
 
 	(void) ms;
-	i = 0;
-	nbr = atoll(cmd->args[1]);
+	//i = 0;
+
+	nbr = ft_atoll(cmd->args[1]);
 	if (nbr >= LLONG_MIN && nbr <= LLONG_MAX)
 	{
-		while (cmd->args[1][i] != '\0')
+		if (nbr == 0 && ft_strcmp(cmd->args[1], "0") != 0)
 		{
-			if (!ft_is_valid_number(cmd->args[1][i++]))
-			{
-				printf("minishell: exit: %s: numeric argument required\n", cmd->args[1]);
-				return (false);
-			}
+			printf("minishell: exit: %s: numeric argument required\n", cmd->args[1]);
+			return (false);
 		}
 	}
 	else
@@ -38,3 +36,6 @@ bool	ft_exit_validation(t_ms *ms, t_command *cmd)
 	}
 	return (true);
 }
+// LLONG_MIN -9223372036854775808
+// LLONG_MAX 9223372036854775807
+// ULLONG MAX 18446744073709551615
