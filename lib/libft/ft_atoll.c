dcc6367de8e81ab98f43c041b5d4108b72e2c672
Luiza <luzilio.p@gmail.com>
2023-07-26 12:49:45 +0100

    attoc working

--- lib/libft/ft_atoll.c
+++ lib/libft/ft_atoll.c
@@ -3,39 +3,74 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoll.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ialves-m <ialves-m@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lpicoli- <lpicoli-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 12:43:55 by lpicoli-          #+#    #+#             */
-/*   Updated: 2023/07/26 06:51:01 by ialves-m         ###   ########.fr       */
+/*   Updated: 2023/07/26 12:45:59 by lpicoli-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-long long	ft_atoll(const char *nptr)
-{
-	long long	r;
-	int	i;
-	int	signal;
+// long long	ft_atoll(const char *nptr)
+// {
+// 	long long	r;
+// 	int	i;
+// 	int	signal;
 
-	r = 0;
-	i = 0;
-	signal = 1;
-	while (ft_isspace(nptr[i]))
-		i++;
-	if (ft_isnegative(nptr[i]) || ft_ispositive(nptr[i]))
-	{
-		if (ft_isnegative(nptr[i++]))
-		signal = -1;
-	}
-	while (nptr[i] >= '0' && nptr[i] <= '9')
-	{
-		r *= 10;
-		r += nptr[i] - '0';
-		i++;
-	}
-	return (r * signal);
+// 	r = 0;
+// 	i = 0;
+// 	signal = 1;
+// 	while (ft_isspace(nptr[i]))
+// 		i++;
+// 	if (ft_isnegative(nptr[i]) || ft_ispositive(nptr[i]))
+// 	{
+// 		if (ft_isnegative(nptr[i++]))
+// 		signal = -1;
+// 	}
+// 	while (nptr[i] >= '0' && nptr[i] <= '9')
+// 	{
+// 		r *= 10;
+// 		r += nptr[i] - '0';
+// 		i++;
+// 	}
+// 	return (r * signal);
+// }
+
+#include <limits.h>
+
+long long ft_atoll(const char *nptr) {
+    long long r;
+    int i;
+    int signal;
+
+    r = 0;
+    i = 0;
+    signal = 1;
+
+    while (ft_isspace(nptr[i]))
+        i++;
+
+    if (ft_isnegative(nptr[i]) || ft_ispositive(nptr[i])) {
+        if (ft_isnegative(nptr[i++]))
+            signal = -1;
+    }
+
+    while (nptr[i] >= '0' && nptr[i] <= '9') {
+        // Check for overflow before multiplying by 10 and adding the new digit
+        if (r > (LLONG_MAX - (nptr[i] - '0')) / 10) {
+            // Overflow detected
+            return (0);
+        }
+
+        r *= 10;
+        r += nptr[i] - '0';
+        i++;
+    }
+
+    return (r * signal);
 }
+
 
 /* int main()
 {
--- src/builtins/ft_exit_validation.c
+++ src/builtins/ft_exit_validation.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exit_validation.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ialves-m <ialves-m@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lpicoli- <lpicoli-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 13:05:23 by lpicoli-          #+#    #+#             */
-/*   Updated: 2023/07/26 12:30:25 by ialves-m         ###   ########.fr       */
+/*   Updated: 2023/07/26 12:48:42 by lpicoli-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,14 @@
 
 bool	ft_exit_validation(t_ms *ms, t_command *cmd)
 {
-	//int		i;
 	long long int	nbr;
 
 	(void) ms;
-	//i = 0;
-
 	nbr = ft_atoll(cmd->args[1]);
-	if (nbr >= LLONG_MIN && nbr <= LLONG_MAX)
-	{
-		if (nbr == 0 && ft_strcmp(cmd->args[1], "0") != 0)
-		{
-			printf("minishell: exit: %s: numeric argument required\n", cmd->args[1]);
-			return (false);
-		}
-	}
-	else
+	if (nbr == 0 && ft_strcmp(cmd->args[1], "0") != 0)
 	{
 		printf("minishell: exit: %s: numeric argument required\n", cmd->args[1]);
 		return (false);
 	}
 	return (true);
 }
-// LLONG_MIN -9223372036854775808
-// LLONG_MAX 9223372036854775807
-// ULLONG MAX 18446744073709551615
