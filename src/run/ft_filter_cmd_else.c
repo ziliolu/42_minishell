a5aa3f76ff39f07a45f3ede376fe59ffbf38e167
Luiza <luzilio.p@gmail.com>
2023-07-30 19:59:39 +0100

    fix smacc errors

--- src/run/ft_filter_cmd_else.c
+++ src/run/ft_filter_cmd_else.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_filter_cmd_else.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ialves-m <ialves-m@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lpicoli- <lpicoli-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 10:01:08 by lpicoli-          #+#    #+#             */
-/*   Updated: 2023/07/27 13:02:43 by ialves-m         ###   ########.fr       */
+/*   Updated: 2023/07/30 19:15:14 by lpicoli-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ void	ft_filter_cmd_else(t_ms *ms, t_command *cmd)
 		ft_reset_fd_in_out(ms);
 		if (ms->n_pipes != 0)
 			g_exit_status = 127;
-		if (ft_is_absolute_path(cmd->args[0]))
+		if (ft_is_absolute_path(cmd->args[0]) || !ms->paths)
 			ft_error_var_start("No such file or directory", cmd->args[0], 2);
 		else
 		{
