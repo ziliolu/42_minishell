30ea38377ae52b0464bb72c19976326ac9fef5d2
Pastilhex <ivodanielmarques@gmail.com>
2023-08-06 11:05:59 +0100

    Norm Ok! Leaks Ok

---
+++ .history/src/builtins/cd/ft_cd_is_chdir_20230806051713.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_cd_is_chdir.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ialves-m <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/21 20:51:00 by ialves-m          #+#    #+#             */
+/*   Updated: 2023/08/06 05:17:13 by ialves-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../includes/minishell.h"
+
+void	ft_modify_oldpwd(t_ms *ms, t_cd *cd)
+{
+	char	*tmp;
+
+	tmp = NULL;
+	if (ft_is_already_in_list("OLDPWD", ms->ms_env))
+		ft_update_list(ms->ms_env, "OLDPWD", cd->oldpwd);
+	else
+	{
+		if (ft_is_already_in_list("PWD", *ms->vars))
+			tmp = ft_strjoin("OLDPWD=", ft_return_list_info(*ms->vars, "PWD"));
+		else if (ft_is_already_in_list("PWD", ms->ms_env))
+			tmp = ft_strjoin("OLDPWD=", ft_return_list_info(ms->ms_env, "PWD"));
+		ft_add_node_to_list(ms, ms->vars, tmp);
+		ft_free(tmp);
+	}
+}
+
+void	ft_modify_pwd(t_ms *ms, t_cd *cd)
+{
+	char
+	if (ft_is_already_in_list("PWD", ms->ms_env))
+		ft_update_list(ms->ms_env, "PWD", cd->pwd);
+	if (!ft_is_already_in_list("PWD", *ms->vars))
+	{
+		
+		ft_add_node_to_list(ms, ms->vars, ft_strjoin("PWD=", ft_getcwd()));
+	}
+	else
+		ft_update_list(*ms->vars, "PWD", cd->pwd);
+}
+
+void	ft_cd_is_chdir(t_ms *ms, t_command *cmd, t_cd *cd)
+{
+	if (chdir(cd->pwd) == 0 && ((ms->spaces_flag <= 1)
+			|| ft_is_home_path(cmd->args[1])))
+	{
+		ft_modify_oldpwd(ms, cd);
+		ft_modify_pwd(ms, cd);
+	}
+	else if (cd->path && cd->path[0] == '$' && ms->spaces_flag == 1)
+		chdir(ft_return_list_info(ms->ms_env, "HOME"));
+	else if (ms->spaces_flag > 1)
+		ft_error(ms, "cd: too many arguments", NULL, 1);
+	else
+	{
+		ft_printf("minishell: cd: %s: no such file or directory\n", cd->path);
+		g_exit_status = 1;
+	}
+}
---
+++ .history/src/pipes/ft_init_pipes_20230806044531.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_init_pipes.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ialves-m <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/16 10:01:08 by lpicoli-          #+#    #+#             */
+/*   Updated: 2023/08/06 04:45:31 by ialves-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+void	ft_init_pipes(t_ms *ms)
+{
+	int	i;
+
+	i = 0;
+	if (ms->std_in_arr)
+		free()
+	ms->std_in_arr = ft_calloc(ms->n_pipes * 2 + 1, sizeof(int));
+	ms->std_out_arr = ft_calloc(ms->n_pipes * 2 + 1, sizeof(int));
+	while (ms->cmds[i].type)
+	{
+		if (ms->cmds[i].type == PIPE_LINE)
+			pipe(ms->cmds[i].fd);
+		ms->std_in_arr[i] = dup(STDIN_FILENO);
+		ms->std_out_arr[i] = dup(STDOUT_FILENO);
+		i++;
+	}
+}
