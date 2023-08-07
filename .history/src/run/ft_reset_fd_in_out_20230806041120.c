16ee664a79323339cd44254340bf69f7b8c2676a
Pastilhex <ivodanielmarques@gmail.com>
2023-08-07 04:56:10 +0100

    heredoc not working

--- .history/src/run/ft_reset_fd_in_out_20230806041120.c
+++ .history/src/run/ft_reset_fd_in_out_20230806041120.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_reset_fd_in_out.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ialves-m <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: lpicoli- <lpicoli-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 10:01:08 by lpicoli-          #+#    #+#             */
-/*   Updated: 2023/08/07 04:45:06 by ialves-m         ###   ########.fr       */
+/*   Updated: 2023/08/05 15:50:12 by lpicoli-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,10 @@ void	ft_reset_fd_in_out(t_ms *ms)
 {
 	dup2(ms->std_out, STDOUT_FILENO);
 	dup2(ms->std_in, STDIN_FILENO);
+}
+
+void	ft_reset_heredoc(t_ms *ms, int i)
+{
+	dup2(ms->std_out_arr[i], STDOUT_FILENO);
+	dup2(ms->std_in_arr[i], STDIN_FILENO);
 }
--- .history/src/run/ft_reset_fd_in_out_20230807044503.c
+++ .history/src/run/ft_reset_fd_in_out_20230807044503.c
@@ -6,7 +6,7 @@
 /*   By: ialves-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 10:01:08 by lpicoli-          #+#    #+#             */
-/*   Updated: 2023/08/07 04:45:06 by ialves-m         ###   ########.fr       */
+/*   Updated: 2023/08/07 04:45:03 by ialves-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,3 +17,4 @@ void	ft_reset_fd_in_out(t_ms *ms)
 	dup2(ms->std_out, STDOUT_FILENO);
 	dup2(ms->std_in, STDIN_FILENO);
 }
+
