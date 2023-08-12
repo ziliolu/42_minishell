c3b35fe9c5dbcf8cb63973a47bc6939e5f366ec0
Pastilhex <ivodanielmarques@gmail.com>
2023-08-12 07:01:40 +0100

    exit and exit 1 Ok

--- src/builtins/exit/ft_exit.c
+++ src/builtins/exit/ft_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ialves-m <ialves-m@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ialves-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 13:05:23 by lpicoli-          #+#    #+#             */
-/*   Updated: 2023/08/11 21:04:13 by ialves-m         ###   ########.fr       */
+/*   Updated: 2023/08/12 07:01:03 by ialves-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 
 void	ft_exit(t_ms *ms, t_command *cmd)
 {
+	int	status;
+
 	if (cmd->args[1] && ft_exit_validation(ms, cmd))
 	{
+		status = ft_atoi(cmd->args[1]);
 		if (ms->n_pipes == 0)
 		{
 			printf("exit\n");
@@ -28,9 +31,9 @@ void	ft_exit(t_ms *ms, t_command *cmd)
 			free(ms->count_args);
 			ft_free_elem_list(*ms->lexed_list);
 			free(ms->lexed_list);
-			free(ms->clean_read_content);
-			// ft_free_cmds(ms);
-			exit(ft_atoi(cmd->args[1]));
+			free(ms->clean);
+			ft_free_cmds(ms);
+			exit(status);
 		}
 		else
 			g_exit_status = ft_atoi(cmd->args[1]);
@@ -47,7 +50,7 @@ void	ft_exit(t_ms *ms, t_command *cmd)
 		free(ms->count_args);
 		ft_free_elem_list(*ms->lexed_list);
 		free(ms->lexed_list);
-		free(ms->clean_read_content);
+		free(ms->clean);
 		ft_free_cmds(ms);
 		exit (g_exit_status);
 	}
